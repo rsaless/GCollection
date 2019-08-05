fc1642dc624788b1ce8fc018d2cf3aafe59cf2a0
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-08-05 01:11:18 -0300

    Cadastrando novos usuários

--- api/Routes/users.js
+++ api/Routes/users.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../model/user');
 
 router.get('/',(req, res) => {
-    let obj = req.query;
-    return res.send({message: "tudo ok com o metodo GET no users"});
-})
-router.post('/',(req, res) => {
-    let obj = req.query;
-    return res.send({message: "tudo ok com o metodo POST no users"});
-})
+    Users.find({}, (err, data) => {
+        if (err) return res.send({error: "Erro na consulta de usuários"})
+        return res.send(data);
+    });
+});
+
+router.post('/create',(req, res) => {
+    let {email, password} = req.body;
+    if (!email || !password) return res.send({error: "Dados insuficientes"});
+    Users.findOne({email}, (err, data) => {
+        if (err) return res.send({error: "Erro ao buscar usuário"});
+        if (data) return res.send({error: "Usuário já registrado"});
+        Users.create(req.body, (err, data) => {
+            if (err) return res.send({error: "Erro ao criar usuário"});
+            data.password = undefined;
+            return res.send(data);
+        });
+    });
+});
 
 module.exports = router;
