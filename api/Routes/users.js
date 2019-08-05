74531f03c25c713c4ce157b03cac067a324251c1
Rafael Sales Pavarina <rspavarina@gmail.com>
2019-08-05 00:35:04 -0300

    Configurando o Mongoose

--- api/Routes/users.js
+++ api/Routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-app.get('/',(req, res) => {
+router.get('/',(req, res) => {
     let obj = req.query;
     return res.send({message: "tudo ok com o metodo GET no users"});
 })
-app.post('/',(req, res) => {
+router.post('/',(req, res) => {
     let obj = req.query;
     return res.send({message: "tudo ok com o metodo POST no users"});
 })
--- api/app.js
+++ api/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//Mongo
+const bd_url = 'mongodb+srv://rafael_admin:naopodeadmin123456@gcdb-axygf.mongodb.net/test?retryWrites=true&w=majority';
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true};
+mongoose.connect(bd_url,options);
+mongoose.set('useCreateIndex', true);
+mongoose.connection.on('error', err => console.log("Erro na conexão com o BD: " + err));
+mongoose.connection.on('disconnected', () => console.log("Aplicação desconectada do BD"));
+mongoose.connection.on('connected', () => console.log("Aplicação conectada do BD"));
+
+//Body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
 const indexRoutes = require('./Routes/index');
 const usersRoutes = require('./Routes/users');
-
 app.use('/', indexRoutes);
 app.use('/users', usersRoutes);
 
-app.listen(3000);
 
+app.listen(3000);
 module.exports = app;
